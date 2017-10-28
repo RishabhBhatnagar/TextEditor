56f4bab988a806230c60e0c538c2a02f5dcf289b
meandmyprogram <bhatnagarrishabh4@gmail.com>
2017-10-28 15:53:19 +0530

    second commit

--- Frame.py
+++ Frame.py
@@ -1,12 +1,8 @@
 from tkinter import *
-
 root = Tk()
 frame = Frame(root)
 frame.pack()
-
 bottomframe = Frame(root)
-bottomframe.pack( side = BOTTOM )
-
-
-
+root.title("jilukjhfgd")
+root.geometry("400x500")
 root.mainloop()
