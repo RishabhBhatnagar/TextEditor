609efcee54208331ad8da84a0d71721b91a18a0c
sfit-Rishabh <bhatnagarrishabh4@gmail.com>
2017-10-28 18:39:00 +0530

    "fourth commit: added menus"

--- Frame.py
+++ Frame.py
@@ -1,8 +1,47 @@
 from tkinter import *
+
 root = Tk()
-frame = Frame(root)
-frame.pack()
-bottomframe = Frame(root)
+
 root.title("Text Editor")
-root.geometry("400x500")
+
+#Menu (toolbar)
+menu = Menu(root)
+root.config(menu=menu)
+
+fileMenu = Menu(menu)
+menu.add_cascade(label="File", menu=fileMenu)
+fileMenu.add_command(label="New File")
+fileMenu.add_command(label="Open File")
+
+# SubMenu of OpenRecent
+openRecentMenu = Menu(fileMenu)
+fileMenu.add_cascade(label="Open Recent", menu=openRecentMenu)
+openRecentMenu.add_command(label="Recent file 1")
+openRecentMenu.add_command(label="Recent file 2")
+openRecentMenu.add_command(label="Recent file 3")
+openRecentMenu.add_command(label="Recent file 4")
+openRecentMenu.add_command(label="Recent file 5")
+# SubMenu of OpenRecent END
+
+fileMenu.add_separator();
+fileMenu.add_command(label="Save")
+fileMenu.add_command(label="Save as..")
+fileMenu.add_separator();
+fileMenu.add_command(label="Exit")
+
+editMenu = Menu(menu)
+menu.add_cascade(label="Edit", menu=editMenu)
+editMenu.add_command(label="Undo")
+editMenu.add_command(label="Redo")
+editMenu.add_command(label="Cut")
+editMenu.add_command(label="Copy")
+editMenu.add_command(label="Paste")
+
+findMenu = Menu(menu)
+menu.add_cascade(label="Find", menu=findMenu)
+findMenu.add_command(label="Find all")
+findMenu.add_command(label="Find next")
+findMenu.add_command(label="Find previous")
+#Creating menu END
+
 root.mainloop()
